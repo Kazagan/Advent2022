d0fa869282a85ff709debc80490df1f41f4c1739
Kazagan <steffen.paige@outlook.com>
2022-12-05 20:51:18 -0600

    small restructing

--- src/Days/Day05.cs
+++ src/Days/Day05.cs
@@ -19,13 +19,15 @@ public class Day05
 
         var stacks = BuildStack(start);
         var moves = lines
-            .Skip(i)
-            .Select(x => Regex.Matches(x, "[0-9]+")
+            .Skip(i) // Skip stack setup
+            .Select(x => Regex
+                .Matches(x,
+                    "[0-9]+") // [0-9] matches any number, + matches 1 or more for multi digit numbers. \d not recognized.
                 .Select(y => int.Parse(y.Value)).ToArray());
 
         foreach (var move in moves)
         {
-            Console.WriteLine($"move {move[0]} from {move[1]} to {move[2]}");
+            // Console.WriteLine($"move {move[0]}\tfrom {move[1]}\tto {move[2]}");
             var items = stacks[move[1] - 1].PopRange(move[0]);
             stacks[move[2] - 1].PushRange(items);
         }
@@ -36,10 +38,9 @@ public class Day05
     private List<Stack<char>> BuildStack(List<string> start)
     {
         var output = new List<Stack<char>>();
-        var bottom = start.Count - 2; // 2 to account for empty line between initial setup and moves.
-        var stackCount =
-            start[bottom--]
-                .Max(x => x == ' ' ? 0 : x - '0'); // Get number of stacks, and set bottom to bottom of stacks.
+        var bottom = start.Count - 2;
+        //Get number of stacks, and set bottom to bottom of stacks.
+        var stackCount = start[bottom--].Max(x => x == ' ' ? 0 : x - '0'); 
         for (int i = 0; i < stackCount; i++)
             output.Add(new Stack<char>());
 
