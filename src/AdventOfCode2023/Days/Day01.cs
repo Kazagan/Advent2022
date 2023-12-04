f551bdd681a0d000cdd3c95b26a62c4d24b6eb66
Kazagan <steffen.paige@outlook.com>
2023-12-04 11:43:48 -0600

    day 1 done

---
+++ src/AdventOfCode2023/Days/Day01.cs
@@ -0,0 +1,73 @@
+namespace AdventOfCode2023.Days;
+
+public static class Day01
+{
+
+    private static Dictionary<string, string> Numbers => new()
+    {
+        {"one", "1"}, {"two", "2"}, {"three", "3"}, {"four", "4"}, {"five", "5"}, {"six", "6"}, {"seven", "7"}, {"eight", "8"}, {"nine", "9"},
+    };
+    
+    public static int Solution(string file)
+    {
+        var asDigits = File.ReadLines(file)
+            .Select(line => GetFirstNumber(line) + GetLastNumber(line)).ToList();
+        return asDigits
+            .Sum(int.Parse);
+    }
+
+    private static string GetFirstNumber(string input)
+    {
+        for (var i = 0; i < input.Length; i++)
+        {
+            if (char.IsDigit(input[i]))
+            {
+                return input[i].ToString();
+            }
+            
+            var current = input[i..];
+            var keys = Numbers.Keys.Where(x => x.Length <= current.Length).ToList();
+
+            
+            var number = keys.FirstOrDefault(n => n.Equals(current[..n.Length]));
+            if (number is null)
+            {
+                continue;
+            }
+
+            return Numbers[number];
+        }
+
+        return "";
+    }
+    
+    private static string GetLastNumber(string input)
+    {
+        for (var i = input.Length - 1; i >= 0; i--)
+        {
+            var current = input[i..];
+            var keys = Numbers.Keys.Where(x => x.Length <= current.Length).ToList();
+
+            if (char.IsDigit(current.First()))
+            {
+                return current.First().ToString();
+            }
+
+            if (!keys.Any())
+            {
+                continue;
+            }
+            var number = keys.FirstOrDefault(n => n.Equals(current[..n.Length]));
+            if (number is null)
+            {
+                continue;
+            }
+
+            return Numbers[number];
+        }
+
+        return "";
+    }
+
+    private static string AsDigit(this string input) => Numbers.TryGetValue(input, out var result) ? result : input;
+}
---
+++ src/AdventOfCode2023/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+using AdventOfCode2023.Days;
+
+namespace AdventOfCode2023;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var day = "01";
+        var directory = $"TextFiles/Day{day}/";
+        var files = Directory.GetFiles(directory);
+
+        var results = files.ToDictionary(file => file.Split("/").Last(), Day01.Solution);
+        foreach (var result in results)
+        {
+            Console.WriteLine($"{result.Key}: {result.Value}");
+        }
+    }
+}
